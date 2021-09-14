6375d90b0bcb4c7f3881a518f185e5753b70d3c1
nate-silver <32363441+nate-silver@users.noreply.github.com>
2021-09-14 14:53:54 +0800

    Tidied up lnkfile.py

--- lnkfiles.py
+++ lnkfiles.py
@@ -7,48 +7,12 @@ OFFICE_RECENT_LNK_FILES = '{}\\AppData\\Roaming\\Microsoft\\Office\\Recent'.form
 
 class Lnkfile:
 	def __init__(self):
-		self.local_base_path = ""
-		self.accessed_time = ""
-		self.creation_time = ""
-		self.modified_time = ""
-		self.drive_serial_no = ""
-		self.drive_type = ""
-
-	def set_local_base_path(self, data):
-		self.local_base_path = data
-
-	def set_accessed_time(self, data):
-		self.accessed_time = data
-
-	def set_creation_time(self, data):
-		self.creation_time = data
-
-	def set_modified_time(self, data):
-		self.modified_time = data
-
-	def set_drive_serial_no(self, data):
-		self.drive_serial_no = data
-
-	def set_drive_type(self, data):
-		self.drive_type = data
-
-	def get_local_base_path(self):
-		return self.local_base_path
-
-	def get_accessed_time(self):
-		return self.accessed_time
-
-	def get_creation_time(self):
-		return self.creation_time
-
-	def get_modified_time(self):
-		return self.modified_time
-
-	def get_drive_serial_no(self):
-		return self.drive_serial_no
-
-	def get_drive_type(self):
-		return self.drive_type
+		self.local_base_path = None
+		self.accessed_time = None
+		self.creation_time = None
+		self.modified_time = None
+		self.drive_serial_no = None
+		self.drive_type = None
 
 
 def parse_lnk_files(path, filename):
@@ -64,41 +28,41 @@ def parse_lnk_files(path, filename):
 			json_format = lnk_meta.get_json()
 
 			try:
-				lnk_file_obj.set_local_base_path(json_format['link_info']['local_base_path'])
+				lnk_file_obj.local_base_path = json_format['link_info']['local_base_path']
 			except KeyError:
 				pass
 
 			try:
-				lnk_file_obj.set_accessed_time(json_format['header']['accessed_time'])
+				lnk_file_obj.accessed_time = json_format['header']['accessed_time']
 			except KeyError:
 				pass
 
 			try:
-				lnk_file_obj.set_creation_time(json_format['header']['creation_time'])
+				lnk_file_obj.creation_time = json_format['header']['creation_time']
 			except KeyError:
 				pass
 
 			try:
-				lnk_file_obj.set_modified_time(json_format['header']['modified_time'])
+				lnk_file_obj.modified_time = json_format['header']['modified_time']
 			except KeyError:
 				pass
 
 			try:
-				lnk_file_obj.set_drive_serial_no(json_format['link_info']['location_info']['drive_serial_number'])
+				lnk_file_obj.drive_serial_number = json_format['link_info']['location_info']['drive_serial_number']
 			except KeyError:
 				pass 
 
 			try:
-				lnk_file_obj.set_drive_type(json_format['link_info']['location_info']['drive_type'])
+				lnk_file_obj.drive_type = json_format['link_info']['location_info']['drive_type']
 			except KeyError:
 				pass
 
-			line = "{}, {}, {}, {}, {}, {}\n".format(lnk_file_obj.get_local_base_path(), 
-														lnk_file_obj.get_accessed_time(), 
-														lnk_file_obj.get_creation_time(), 
-														lnk_file_obj.get_modified_time(), 
-														lnk_file_obj.get_drive_serial_no(), 
-														lnk_file_obj.get_drive_type())
+			line = "{}, {}, {}, {}, {}, {}\n".format(lnk_file_obj.local_base_path, \
+														lnk_file_obj.accessed_time, \
+														lnk_file_obj.creation_time, \
+														lnk_file_obj.modified_time, \
+														lnk_file_obj.drive_serial_no, \
+														lnk_file_obj.drive_type)
 			file.write(line)
 
 
