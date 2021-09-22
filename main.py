656299c80b4be537e50fce14ce45631a02078417
nate-silver <32363441+nate-silver@users.noreply.github.com>
2021-09-22 14:48:32 +0800

    main.py edited

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import argparse
-from auxillary import *
-from external_device import *
+import importlib
+import os
+
+PLUGIN_PATH = "plugins"
 
 # Argparser
 parser = argparse.ArgumentParser(
@@ -8,8 +10,26 @@ parser = argparse.ArgumentParser(
     epilog="ICT2202 Assignment 1 Team PanzerWerfer"
 )
 requiredNamed = parser.add_argument_group("required arguments")
-# parser.add_argument("-o", help="")
+parser.add_argument("-o", help="")
 args = parser.parse_args()
+
+
+def load_plugins():
+    cwd = os.getcwd()
+    plugin_path = "{}\\{}".format(cwd, PLUGIN_PATH)
+    plugins = os.listdir(plugin_path)
+    print("[+] Loading modules....")
+    for plugin in plugins:
+        print(plugin)
+    print("\n[!] Modules successfully loaded!")
+    return plugin_path, plugins
+
+
+def run_plugins(plugin_path, plugins):
+    for plugin in plugins:
+        plugin = plugin[:-3]
+        plugin_path = "{}.{}".format(PLUGIN_PATH, plugin)
+        importlib.import_module(plugin_path)
 
 
 def print_banner():
@@ -32,7 +52,9 @@ def main():
     print("-" * 50)
     print_banner()
     print("-" * 50)
-    get_portable_devices()
+    plugin_path, plugins = load_plugins()
+    run_plugins(plugin_path, plugins)
+    
 
 
 if __name__ == '__main__':
