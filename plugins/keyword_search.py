9127045def3da892cbd09fa55ff87c24af8b3c6e
nate-silver <32363441+nate-silver@users.noreply.github.com>
2021-09-27 18:50:08 +0800

    Keyword search done and added encryption library (WIP)

---
+++ plugins/keyword_search.py
@@ -0,0 +1,57 @@
+import os
+import json
+import re
+from main import get_project_root
+from pathlib import Path
+
+TITLE = 0
+DESCRIPTION = 1
+ROOT = str(get_project_root())
+OUTFILE = Path(ROOT + "/data/keyword_search/keyword_search.json")
+
+
+def dump_to_json(file_path, data):
+	with open(file_path, "w") as outfile:
+		json.dump(data, outfile, default=str, indent=4)
+
+
+def run():
+    # Read keywords
+    # keywords = open('keywords.txt', 'r')
+    with open('keywords.txt', 'r') as f:
+        keywords = f.read().splitlines()
+
+    data_root_folder_path = "{}/data".format(get_project_root())
+    data_sub_folders = os.listdir(data_root_folder_path)
+    export_data = []
+
+    # Read data files
+    for sub_folder in data_sub_folders:
+        data_sub_folders_path = "{}/{}".format(data_root_folder_path, sub_folder)
+        data_files = os.listdir(data_sub_folders_path)
+
+        for data_file in data_files:
+            data_file_path = "{}/{}".format(data_sub_folders_path, data_file)
+            with open(data_file_path, 'r') as file:
+                matches = 0
+                loaded_data = json.load(file)
+                export_data.append(loaded_data[0])
+                export_data.append(loaded_data[1])
+                loaded_data.pop(0)
+                loaded_data.pop(0)
+                for entry in loaded_data:
+                    result = ' '.join(filter(None, list(map(str, entry.values()))))
+                    for keyword in keywords:
+                        if(re.search(keyword, result)):
+                            matches += 1
+                            export_data.append(entry)
+                            break
+            if matches == 0:
+                export_data.append({
+                    "not found": "Unable to find any results from the keyword search!"
+                })
+    OUTFILE.parent.mkdir(exist_ok=True, parents=True)
+    dump_to_json(OUTFILE, export_data)
+
+if __name__ == "__main__":
+    run()
